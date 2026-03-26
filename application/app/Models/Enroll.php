a1a8763cbfef172b55ae86b9e1f990e3011c359f
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-03-27 02:49:17 +0500

    Allow mass assignment for enroll user_id

--- application/app/Models/Enroll.php
+++ application/app/Models/Enroll.php
@@ -9,6 +9,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Enroll extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'deposit_id',
+        'owner_id',
+        'owner_type',
+        'name',
+        'discount',
+        'price',
+        'total_amount',
+        'status',
+    ];
     
     public function course(){
         return $this->belongsTo(Course::class);
